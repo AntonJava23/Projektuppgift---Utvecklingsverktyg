bdc63c450ce2b9801a19731279931d999db485c0
AntonJava23 <antbri1009@skola.goteborg.se>
2024-02-09 12:40:42 +0100

    Added variables and a constructor to Product.

--- src/main/java/se/yrgo/productClasses/Product.java
+++ src/main/java/se/yrgo/productClasses/Product.java
@@ -1,4 +1,18 @@
 package se.yrgo.productClasses;
+import java.lang.*;
 
 public class Product {
+    private String productId;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String productId, String name, double price, int quantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+
 }
