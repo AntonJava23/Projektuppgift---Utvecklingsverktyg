c175182cd1115f2e5353ada293b8a7a1f8f5377c
AntonJava23 <antbri1009@skola.goteborg.se>
2024-02-09 14:00:31 +0100

    Made a test class for Product.

--- src/test/java/se/yrgo/ProductTest.java
+++ src/test/java/se/yrgo/ProductTest.java
@@ -1,5 +1,12 @@
 package se.yrgo;
 
+import org.junit.jupiter.api.Test;
+
 public class ProductTest {
 
+    @Test
+    void testCalculateCost() {
+        Product p1 = new Product("23", "Bolts", 23.99, 3);
+
+    }
 }
