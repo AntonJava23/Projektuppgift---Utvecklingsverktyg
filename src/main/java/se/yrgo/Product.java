9d374af0919843e7191732e877483a1aeca2f837
AntonJava23 <antbri1009@skola.goteborg.se>
2024-02-09 13:59:43 +0100

    Added getters and toString method to Product.

--- src/main/java/se/yrgo/Product.java
+++ src/main/java/se/yrgo/Product.java
@@ -20,4 +20,28 @@ public class Product {
         return cost;
     }
 
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product\n" +
+                "ID: " + productId + "\n" +
+                "Name: " + name + "\n" +
+                "Price: " + price +  "\n" +
+                "Quantity: " + quantity;
+    }
 }
