885a8799e0ce7d94d4453eb5a2d91ba6578b44e5
AntonJava23 <antbri1009@skola.goteborg.se>
2024-02-09 14:00:11 +0100

    Made a Main class and code to print out a product.

---
+++ src/main/java/se/yrgo/ShopMain.java
@@ -0,0 +1,11 @@
+package se.yrgo;
+
+import java.io.PrintStream;
+
+public class ShopMain {
+
+    public static void main(String[] args) {
+        Product product1 = new Product("23", "Bolt", 23.99, 3);
+        System.out.println(product1);
+    }
+}
