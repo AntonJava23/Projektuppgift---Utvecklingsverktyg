30a5fe8ba908af22d703f89a9a481aa44743799a
AntonJava23 <antbri1009@skola.goteborg.se>
2024-02-09 13:41:32 +0100

    Made ProductTest class.

---
+++ src/main/java/se/yrgo/Product.java
@@ -0,0 +1,23 @@
+package se.yrgo;
+
+public class Product {
+    private String productId;
+    private String name;
+    private double price;
+    private int quantity;
+
+
+    public Product(String productId, String name, double price, int quantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double calculateCost(double price, int quantity) {
+        double cost;
+        cost = price * quantity;
+        return cost;
+    }
+
+}
